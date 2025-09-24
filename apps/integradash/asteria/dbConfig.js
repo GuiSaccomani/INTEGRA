499125c88892c289833817763405957af51842fb
unknown <gui.andrade1510@gmail.com>
2025-09-23 21:21:02 -0300

    usr pass

--- apps/integradash/asteria/dbConfig.js
+++ apps/integradash/asteria/dbConfig.js
@@ -2,10 +2,9 @@
 const oracledb = require("oracledb");
 
 const dbConfig = {
-  user: "SYS",        // ex: "system"
-  password: "Authentic12",      // ex: "oracle"
-  connectString: "localhost:1521/XE", // ex: "host:porta/SID ou SERVICE_NAME"
-    privilege: oracledb.SYSDBA
+  user: "INTEGRA",        // ex: "system"
+  password: "1111",      // ex: "oracle"
+  connectString: "localhost:1521/XE" // ex: "host:porta/SID ou SERVICE_NAME"
 };
 
 module.exports = dbConfig;
