a8df6843401d638549705ff392b0f267c8d2ecf7
unknown <gui.andrade1510@gmail.com>
2025-10-15 23:12:33 -0300

    change

--- apps/integradash/asteria/dbConfig.js
+++ apps/integradash/asteria/dbConfig.js
@@ -2,9 +2,9 @@
 const oracledb = require("oracledb");
 
 const dbConfig = {
-  user: "INTEGRA",        // ex: "system"
+  user: "INTEGRA2",        // ex: "system"
   password: "1111",      // ex: "oracle"
-  connectString: "localhost:1521/XE" // ex: "host:porta/SID ou SERVICE_NAME"
+  connectString: "localhost:1521/XEPDB1" // ex: "host:porta/SID ou SERVICE_NAME"
 };
 
 module.exports = dbConfig;
